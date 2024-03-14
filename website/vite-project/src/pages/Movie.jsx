ca49806b8e1a319dc446e5bbf87839560454a7c2
tommytt-ops <tommytt721@gmail.com>
2024-03-14 21:52:39 +0100

    testing

--- website/vite-project/src/pages/Movie.jsx
+++ website/vite-project/src/pages/Movie.jsx
@@ -12,7 +12,7 @@ import {
   Legend,
 } from 'chart.js';
 
-// Register Chart.js components
+
 ChartJS.register(
   PointElement,
   LineElement,
@@ -29,20 +29,20 @@ function CsvDataComponent() {
 
 
   useEffect(() => {
-    // Check if fileName is truthy to avoid unnecessary fetch calls
+    
     if (fileName) {
       fetchDataPoints(fileName)
         .then(points => {
-          setDataPoints(points); // Update state with the fetched data points
+          setDataPoints(points); 
         })
         .catch(error => console.error("Error fetching data points:", error));
     }
-  }, [fileName]); // Depend on fileName to refetch when it changes
+  }, [fileName]); 
 
   const data = {
     labels: dataPoints.map(point => point.time),
     datasets: [{
-      label: selectedMovie ? selectedMovie.replace('.csv', '') : '', // Use file name as label, removing '.csv'
+      label: selectedMovie, 
       data: dataPoints.map(point => point.value),
       fill: false,
       borderColor: 'rgb(75, 192, 192)',
