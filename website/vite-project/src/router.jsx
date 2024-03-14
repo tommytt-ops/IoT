2d453ce8bdab511afefc6fcab0c00783236a0dff
tommytt-ops <tommytt721@gmail.com>
2024-03-14 21:19:42 +0100

    cope

--- website/vite-project/src/router.jsx
+++ website/vite-project/src/router.jsx
@@ -1,22 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Dash from './pages/App';
 import Movie from './pages/Movie'
 
 
 
 const App = () =>  {
-    return (
-      <div className="App">
-          <Router>
-            <Routes>
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route element={<Dash/>} path="/" />
+          <Route element={<Movie/>} path="/movie" />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
 
-                <Route element={<Dash/>} path="/"/>
-                <Route element={<Movie/>} path="/movie"/>
-
-            </Routes>
-        </Router>
-      </div>
-    );
-  }
-  
-  export default App;
+export default App;
